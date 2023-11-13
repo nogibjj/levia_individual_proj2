6e5625819e385c6525eec67bdf2974538cd35002
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-11-13 00:03:08 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,13 @@ impl Database {
         let connection = Connection::open("my_database.db")?;
         Ok(Database { connection })
     }
+    //create data
+    pub fn create_data(&self, data: &str) -> Result<()> {
+        self.connection.execute("INSERT INTO my_table (data_column) VALUES (?1)", &[data])?;
+        Ok(())
+    }
 
+    //read data
     pub fn get_all_data(&self) -> Result<Vec<String>> {
         let mut stmt = self.connection.prepare("SELECT * FROM my_table")?;
         let data_iter = stmt.query_map([], |row| row.get(0))?;
@@ -29,5 +35,17 @@ impl Database {
         }
 
         Ok(result)
+    }
+
+    //update data
+    pub fn update_data(&self, id: i32, new_data: &str) -> Result<()> {
+        self.connection.execute("UPDATE my_table SET data_column = ?1 WHERE id = ?2", &[new_data, &id.to_string()])?;
+        Ok(())
+    }
+
+    //delete data
+    pub fn delete_data(&self, id: i32) -> Result<()> {
+        self.connection.execute("DELETE FROM my_table WHERE id = ?1", &[&id.to_string()])?;
+        Ok(())
     }
 }
