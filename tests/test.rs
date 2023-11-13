eeed87b51bb198538014ea1769f112c3a5f8a620
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-11-13 00:27:31 -0500

    Update test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -1,40 +1,53 @@
 // In tests/test.rs
 
 use my_project::Database; 
-use rusqlite::{Result, NO_PARAMS};
+use rusqlite::Result;
+use std::fs;
 
 // Function to create a new, isolated database for each test
-fn setup_test_db() -> Result<Database> {
-    let db = Database::new_in_memory()?; 
+fn setup_test_db() -> Result<(Database, String)> {
+    // Using a random file name for each test database
+    let db_path = format!("test_db_{}.db", uuid::Uuid::new_v4());
+    let db = Database::new(&db_path)?;
+
     db.connection.execute(
         "CREATE TABLE IF NOT EXISTS my_table (
             id INTEGER PRIMARY KEY,
             data_column TEXT NOT NULL
-        )", NO_PARAMS)?;
-    Ok(db)
+        )", [])?;
+
+    Ok((db, db_path))
+}
+
+// A helper function to clean up the database file after each test
+fn cleanup_test_db(db_path: &str) {
+    let _ = fs::remove_file(db_path);
 }
 
 #[test]
 fn test_create_data() -> Result<()> {
-    let db = setup_test_db()?;
-
+    let (db, db_path) = setup_test_db()?;
+    
     db.create_data("Test data")?;
     let data = db.get_all_data()?;
     assert!(data.contains(&"Test data".to_string()));
 
+    cleanup_test_db(&db_path);
     Ok(())
 }
 
 #[test]
 fn test_read_data() -> Result<()> {
-    let db = setup_test_db()?;
+    let (db, db_path) = setup_test_db()?;
 
     db.create_data("Test data")?;
     let data = db.get_all_data()?;
     assert!(!data.is_empty());
 
+    cleanup_test_db(&db_path);
     Ok(())
 }
+
 
 #[test]
 fn test_update_data() -> Result<()> {
