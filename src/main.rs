9a03f8722a59195144b1d1c45e297c67bb31a345
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-11-13 00:06:50 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
-// main.rs
-
 use my_project::Database;
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let db = Database::new()?;
-    let data = db.get_all_data()?;
 
-    for item in data {
+    // Create data
+    db.create_data("New data")?;
+    println!("Data creation successful.");
+
+    // Read data
+    let data = db.get_all_data()?;
+    println!("Data retrieved:");
+    for item in &data {
+        println!("{}", item);
+    }
+
+
+    let update_id = 1;
+    db.update_data(update_id, "Updated data")?;
+    println!("Data updated successfully for ID {}.", update_id);
+
+    // Read data again to show the update effect
+    let updated_data = db.get_all_data()?;
+    println!("Data after update:");
+    for item in &updated_data {
+        println!("{}", item);
+    }
+
+
+    let delete_id = 1;
+    db.delete_data(delete_id)?;
+    println!("Data deleted successfully for ID {}.", delete_id);
+
+    // Finally, read data again to confirm the delete effect
+    let final_data = db.get_all_data()?;
+    println!("Current data:");
+    for item in &final_data {
         println!("{}", item);
     }
 
